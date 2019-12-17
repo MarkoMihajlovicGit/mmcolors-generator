df90cc8f3459703ab9fb49ca08b6be8a5e763f10
maklud <markoni4@live.com>
2019-12-17 14:19:12 +0100

    Make Functional SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import useToggle from './hooks/useToggle';
+
 import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
@@ -7,91 +9,87 @@ import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/styles';
-
-// important!!! import our own Navbar.css/styles after rc-slider styles
 import 'rc-slider/assets/index.css';
+// important!!! import our own Navbar.css/styles after rc-slider styles
 import styles from './styles/NavbarStyles';
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      format: 'hex',
-      open: false
-    };
-  }
+function Navbar(props) {
+  const [format, setFormat] = useState('hex');
 
-  handleFormatChange = e => {
-    this.setState({ format: e.target.value, open: true }, () => {
-      this.props.handleChange(this.state.format);
-    });
+  const [open, toggle] = useToggle(false);
+
+  const { level, changeLevel, displaySlider, classes, handleChange } = props;
+
+  useEffect(() => {
+    handleChange(format);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [format]);
+
+  const handleFormatChange = e => {
+    setFormat(e.target.value);
+    toggle(true);
   };
 
-  closeSnackbar = () => {
-    this.setState({ open: false });
+  const closeSnackbar = () => {
+    toggle(false);
   };
 
-  render() {
-    const { level, changeLevel, displaySlider, classes } = this.props;
-    const { format, open } = this.state;
-
-    return (
-      <header className={classes.Navbar}>
-        <div className={classes.logo}>
-          <Link to="/">reactcolorpicker</Link>
-        </div>
-        {displaySlider && (
-          <div>
-            <span>Level: {level}</span>
-            <div className={classes.slider}>
-              <Slider
-                defaultValue={level}
-                min={100}
-                max={900}
-                step={100}
-                onAfterChange={changeLevel}
-              />
-            </div>
+  return (
+    <header className={classes.Navbar}>
+      <div className={classes.logo}>
+        <Link to="/">reactcolorpicker</Link>
+      </div>
+      {displaySlider && (
+        <div>
+          <span>Level: {level}</span>
+          <div className={classes.slider}>
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
           </div>
-        )}
-        <div className={classes.selectContainer}>
-          <Select
-            value={format}
-            onChange={this.handleFormatChange}
-            className={classes.menuItem}
-          >
-            <MenuItem value="hex">HEX - #ffffff</MenuItem>
-            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-            <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
-          </Select>
         </div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          open={open}
-          autoHideDuration={2000}
-          message={
-            <span id="message-id">
-              Format Changed to : {format.toUpperCase()} !
-            </span>
-          }
-          ContentProps={{ 'aria-describedby': 'message-id' }}
-          onClose={this.closeSnackbar}
-          action={[
-            <IconButton
-              onClick={this.closeSnackbar}
-              color="inherit"
-              key="close"
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-          ]}
-        />
-      </header>
-    );
-  }
+      )}
+      <div className={classes.selectContainer}>
+        <Select
+          value={format}
+          onChange={handleFormatChange}
+          className={classes.menuItem}
+        >
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
+        </Select>
+      </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={open}
+        autoHideDuration={2000}
+        message={
+          <span id="message-id">
+            Format Changed to : {format.toUpperCase()} !
+          </span>
+        }
+        ContentProps={{ 'aria-describedby': 'message-id' }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
+    </header>
+  );
 }
 export default withStyles(styles)(Navbar);
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 import ColorBox from './ColorBox';
@@ -6,16 +6,10 @@ import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 import styles from './styles/SingleColorPaletteStyles';
 
-class SingleColorPalette extends Component {
-  constructor(props) {
-    super(props);
-    this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-    this.state = {
-      format: 'hex'
-    };
-  }
+function SingleColorPalette({ palette, colorId, classes }) {
+  const [format, setFormat] = useState('hex');
 
-  gatherShades = (palette, colorToFilterBy) => {
+  const gatherShades = (palette, colorToFilterBy) => {
     //return all shades of given color
     let shades = [];
 
@@ -28,37 +22,35 @@ class SingleColorPalette extends Component {
 
     return shades.slice(1);
   };
+  const _shades = gatherShades(palette, colorId);
 
-  changeFormat = val => {
-    this.setState({ format: val });
+  const changeFormat = val => {
+    setFormat(val);
   };
-  render() {
-    const { format } = this.state;
-    const { paletteName, emoji, id } = this.props.palette;
-    const { classes } = this.props;
 
-    const colorBoxes = this._shades.map(shade => (
-      <ColorBox
-        key={shade.name}
-        name={shade.name}
-        background={shade[format]}
-        showingFullPalette={false}
-      ></ColorBox>
-    ));
-    return (
-      <div className={classes.Palette}>
-        <Navbar handleChange={this.changeFormat} displaySlider={false}></Navbar>
+  const { paletteName, emoji, id } = palette;
 
-        <div className={classes.PaletteColors}>
-          {colorBoxes}
-          <div className={classes.goBack}>
-            <Link to={`/palette/${id}`}>GO BACK</Link>
-          </div>
+  const colorBoxes = _shades.map(shade => (
+    <ColorBox
+      key={shade.name}
+      name={shade.name}
+      background={shade[format]}
+      showingFullPalette={false}
+    ></ColorBox>
+  ));
+  return (
+    <div className={classes.Palette}>
+      <Navbar handleChange={changeFormat} displaySlider={false}></Navbar>
+
+      <div className={classes.PaletteColors}>
+        {colorBoxes}
+        <div className={classes.goBack}>
+          <Link to={`/palette/${id}`}>GO BACK</Link>
         </div>
-        <PaletteFooter paletteName={paletteName} emoji={emoji}></PaletteFooter>
       </div>
-    );
-  }
+      <PaletteFooter paletteName={paletteName} emoji={emoji}></PaletteFooter>
+    </div>
+  );
 }
 
 export default withStyles(styles)(SingleColorPalette);
